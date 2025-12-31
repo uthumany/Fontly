540f63661e1f520425fa1433185cba1c9925cee7
uthumany <129793568+uthumany@users.noreply.github.com>
2025-12-31 15:50:19 -0500

    Add Progressive Web App (PWA) with font preview gallery

---
+++ public/sw.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = 'fontly-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/fonts/fonts.css',
+  '/fonts/ttf/CinzelDecorative-Bold.ttf',
+  '/fonts/ttf/EBGaramond12-Regular.ttf',
+  '/fonts/otf/Goodly-Regular.otf'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE).catch(() => {
+        console.log('Some assets could not be cached');
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type === 'error') {
+          return response;
+        }
+
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      }).catch(() => {
+        return caches.match('/index.html');
+      });
+    })
+  );
+});
